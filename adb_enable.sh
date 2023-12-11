c94fbb213137423e8a34f93b4c7784b9f7d0586f
ju5t3nc4s3 <79302448+ju5t3nc4s3@users.noreply.github.com>
2023-12-11 00:20:15 -0600

    Update adb_enable.sh

--- adb_enable.sh
+++ adb_enable.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
+
 settings put global development_settings_enabled 1
+settings put global adb_enabled 1
+settings put global adb_wifi_enabled 1
+settings put global verifier_verify_adb_installs 0
